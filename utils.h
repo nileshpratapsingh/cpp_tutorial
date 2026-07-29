4d531ccdfc2b230292945f7882d825f63793502f
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-07-29 21:36:57 +0530

    PRAGMA once

--- utils.h
+++ utils.h
@@ -2,16 +2,16 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <cmath>
-#include <vector>
-#include <cstring>
-#include <cstdlib>
-#include <sstream>
-#include <cstring>
-#include <iostream>
-#include <limits>    // Added
-#include <utility>   // Added for std::pair
-#include <boost/multiprecision/cpp_int.hpp>
+#pragma once
+#include <cmath>                            // IWYU pragma: export
+#include <vector>                           // IWYU pragma: export
+#include <cstring>                          // IWYU pragma: export
+#include <cstdlib>                          // IWYU pragma: export
+#include <sstream>                          // IWYU pragma: export
+#include <iostream>                         // IWYU pragma: export
+#include <limits>                           // IWYU pragma: export
+#include <utility>                          // IWYU pragma: export
+#include <boost/multiprecision/cpp_int.hpp> // IWYU pragma: export
 
 void print_array(int arr[], int size);
 

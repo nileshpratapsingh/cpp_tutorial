cc36908fed49f2422ce63225ff5bf76b84c0d547
Nilesh Pratap Singh <nileshpratap190902@gmail.com>
2026-08-02 13:29:26 +0530

    more modules

--- utils.h
+++ utils.h
@@ -6,20 +6,25 @@
 #define RAND_RANGE(min, max) (rand() % ((max) - (min) + 1) + (min))
 
 #pragma once
+
 #include <boost/multiprecision/cpp_int.hpp> // IWYU pragma: export
-#include <cmath>                            // IWYU pragma: export
-#include <cstddef>                          // IWYU pragma: export
-#include <cstdlib>                          // IWYU pragma: export
-#include <cstring>                          // IWYU pragma: export
-#include <ctime>                            // IWYU pragma: export
-#include <iostream>                         // IWYU pragma: export
-#include <limits>                           // IWYU pragma: export
-#include <sstream>                          // IWYU pragma: export
-#include <utility>                          // IWYU pragma: export
-#include <vector>                           // IWYU pragma: export
-#include <windows.h>                        // IWYU pragma: export
+#include <chrono>
+#include <cmath>    // IWYU pragma: export
+#include <cstddef>  // IWYU pragma: export
+#include <cstdlib>  // IWYU pragma: export
+#include <cstring>  // IWYU pragma: export
+#include <ctime>    // IWYU pragma: export
+#include <iostream> // IWYU pragma: export
+#include <limits>   // IWYU pragma: export
+#include <sstream>  // IWYU pragma: export
+#include <thread>
+#include <utility>   // IWYU pragma: export
+#include <vector>    // IWYU pragma: export
+#include <windows.h> // IWYU pragma: export
 
 void print_array(int arr[], int size);
 void initRandom();
 int randomInt(int max);
 int randomInt(int min, int max);
+float randomFloat(float max);
+float randomFloat(float min, float max);
